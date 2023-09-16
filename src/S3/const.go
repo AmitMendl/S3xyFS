27085af792ccf081bd2b3fb9bedbca2ab54a34ef
AmitMendl <amit@mendelevitch@gmail.com>
2023-09-16 23:22:18 +0300

    added acl

--- src/S3/const.go
+++ src/S3/const.go
@@ -1,5 +1,14 @@
 package s3
 
+// ACL
+
+const (
+	ACL_PRIVATE            string = "private"
+	ACL_PUBLIC_READ               = "public-read"
+	ACL_PUBLIC_READ_WRITE         = "public-read-write"
+	ACL_AUTHENTICATED_READ        = "authenticated-read"
+)
+
 // COMMAND CONSTS
 
 // createBucket
